acfb37562a3409faf7d9436fa6c425e21317ee8f
domseichter <domseichter@web.de>
2012-08-30 22:25:21 +0200

    Unit-testing for the exception classes, changes the way the format string is build in IllegalStateOfArgumentException

---
+++ modules/quality-check/src/test/java/com/github/quality/check/exception/IllegalRangeExceptionTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright 2012 André Rouél
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.github.quality.check.exception;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IllegalRangeExceptionTest {
+
+	@Test
+	public void construct_withArgs_successful() {
+		new IllegalRangeException(5, 4, 3);
+	}
+
+	@Test
+	public void construct_withZeroArgs_successful() {
+		new IllegalRangeException(0, 0, 0);
+	}
+
+	@Test
+	public void construct_withArgsAndNullCause() {
+		new IllegalRangeException(2, 1, 3, null);
+	}
+
+	@Test
+	public void construct_withFilledArgsAndFilledCause() {
+		new IllegalRangeException(2, 1, 3, new NumberFormatException());
+	}
+
+	@Test
+	public void construct_withFilledArgsAndNullCause() {
+		final IllegalRangeException e = new IllegalRangeException(2, 1, 3, null);
+		Assert.assertEquals("Arguments start='2', end='1' and size='3' must be a valid range.", e.getMessage());
+	}
+
+	@Test
+	public void construct_withFilledCause() {
+		new IllegalRangeException(new NumberFormatException());
+	}
+
+	@Test
+	public void construct_withNullCause() {
+		new IllegalRangeException((Throwable) null);
+	}
+
+	@Test
+	public void construct_withoutArgs_successful() {
+		new IllegalRangeExceptionTest();
+	}
+
+	@Test
+	public void construct_withoutArgs_successfulAndCheckMessage() {
+		final IllegalRangeException e = new IllegalRangeException();
+		Assert.assertEquals("Arguments must be a valid range.", e.getMessage());
+	}
+}
---
+++ modules/quality-check/src/test/java/com/github/quality/check/exception/IllegalStateOfArgumentsExceptionTest.java
@@ -0,0 +1,67 @@
+package com.github.quality.check.exception;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IllegalStateOfArgumentsExceptionTest {
+	
+
+	@Test
+	public void construct_withArgs_successful() {
+		new IllegalStateOfArgumentException("a^2 + b^2 = c^2");
+	}
+
+	@Test
+	public void message_without_template() {
+		final IllegalStateOfArgumentException e = new IllegalStateOfArgumentException("a=%d");
+		Assert.assertEquals("The passed arguments have caused an invalid state: a=%d", e.getMessage());
+	}
+	
+	@Test
+	public void construct_withArgsAndNullCause() {
+		new IllegalStateOfArgumentException("a^2 + b^2 = c^2", (Throwable)null);
+	}
+
+	@Test
+	public void construct_withArgsAndNullCauseAndFormatValue() {
+		new IllegalStateOfArgumentException("%d^2 + %d^2 = %d^2", 1, 2, 3, (Throwable)null);
+	}
+	
+	@Test
+	public void construct_withFilledArgsAndFilledCause() {
+		new IllegalStateOfArgumentException("a^2 + b^2 = c^2", new NumberFormatException());
+	}
+
+	@Test
+	public void construct_withFilledArgsAndNullCause() {
+		final IllegalStateOfArgumentException e = new IllegalStateOfArgumentException("a != b", (Throwable)null);
+		Assert.assertEquals("The passed arguments have caused an invalid state: a != b", e.getMessage());
+	}
+
+	@Test
+	public void construct_withFilledArgsAndNullCauseAndFormatValue() {
+		final IllegalStateOfArgumentException e = new IllegalStateOfArgumentException("%d^2 + %d^2 = %d^2", 1, 2, 3, (Throwable)null);
+		Assert.assertEquals("1^2 + 2^2 = 3^2", e.getMessage());
+	}
+	
+	@Test
+	public void construct_withFilledCause() {
+		new IllegalStateOfArgumentException(new NumberFormatException());
+	}
+
+	@Test
+	public void construct_withNullCause() {
+		new IllegalStateOfArgumentException((Throwable) null);
+	}
+
+	@Test
+	public void construct_withoutArgs_successful() {
+		new IllegalStateOfArgumentException();
+	}
+
+	@Test
+	public void construct_withoutArgs_successfulAndCheckMessage() {
+		final IllegalStateOfArgumentException e = new IllegalStateOfArgumentException();
+		Assert.assertEquals("Arguments must be valid with the current state.", e.getMessage());
+	}	
+}
