14df981cd8339e57d0ef58cd4501f5b40c7c2912
André Rouél <andre.rouel@googlemail.com>
2012-08-30 19:04:08 +0200

    Added unit tests for class IllegalNullArgumentException

---
+++ modules/quality-check/src/test/java/com/github/quality/check/exception/IllegalNullArgumentExceptionTest.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright 2012 André Rouél
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.github.quality.check.exception;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IllegalNullArgumentExceptionTest {
+
+	@Test
+	public void construct_withArgName_successful() {
+		new IllegalNullArgumentException("argName");
+	}
+
+	@Test
+	public void construct_withEmptyArgName_successful() {
+		new IllegalNullArgumentException("");
+	}
+
+	@Test
+	public void construct_withEmptyArgNameAndNullCause() {
+		new IllegalNullArgumentException("", null);
+	}
+
+	@Test
+	public void construct_withFilledArgNameAndFilledCause() {
+		new IllegalNullArgumentException("argName", new NumberFormatException());
+	}
+
+	@Test
+	public void construct_withFilledArgNameAndNullCause() {
+		final IllegalNullArgumentException e = new IllegalNullArgumentException("argName", null);
+		Assert.assertEquals("Argument 'argName' must not be null.", e.getMessage());
+	}
+
+	@Test
+	public void construct_withFilledCause() {
+		new IllegalNullArgumentException(new NumberFormatException());
+	}
+
+	@Test
+	public void construct_withNullArgName() {
+		new IllegalNullArgumentException((String) null);
+	}
+
+	@Test
+	public void construct_withNullArgNameAndNullCause() {
+		new IllegalNullArgumentException((String) null, null);
+	}
+
+	@Test
+	public void construct_withNullCause() {
+		new IllegalNullArgumentException((Throwable) null);
+	}
+
+	@Test
+	public void construct_withoutArgs_successful() {
+		new IllegalNullArgumentException();
+	}
+
+}
