c72713f978884da3d62ab6fd1b8d6b3fdef95435
André Rouél <andre.rouel@googlemail.com>
2012-10-27 14:39:38 +0200

    Separated out unit tests for all Check.matchesPattern() methods in own test class

--- modules/quality-check/src/test/java/net/sf/qualitycheck/CheckTest.java
+++ modules/quality-check/src/test/java/net/sf/qualitycheck/CheckTest.java
@@ -17,10 +17,8 @@
 package net.sf.qualitycheck;
 
 import java.lang.reflect.Constructor;
-import java.util.regex.Pattern;
 
 import net.sf.qualitycheck.exception.IllegalNullArgumentException;
-import net.sf.qualitycheck.exception.IllegalPatternArgumentException;
 import net.sf.qualitycheck.exception.IllegalPositionIndexException;
 
 import org.junit.Assert;
@@ -77,67 +75,6 @@ public class CheckTest {
 		final Constructor<Check> constructor = Check.class.getDeclaredConstructor();
 		constructor.setAccessible(true);
 		constructor.newInstance();
-	}
-
-	@Test(expected = IllegalNullArgumentException.class)
-	public void matchesPattern_chars_isNull() {
-		Check.matchesPattern(Pattern.compile("abc"), null);
-	}
-
-	@Test(expected = IllegalNullArgumentException.class)
-	public void matchesPattern_pattern_isNull() {
-		Check.matchesPattern(null, "abc");
-	}
-
-	@Test
-	public void matchesPattern_stringbuilder_isValid() {
-		final StringBuilder builder = new StringBuilder();
-		builder.append("abc");
-		Check.matchesPattern(Pattern.compile("abc"), builder);
-	}
-
-	@Test(expected = IllegalPatternArgumentException.class)
-	public void matchesPattern_text_isInvalid() {
-		final String text = "def";
-		Assert.assertSame(text, Check.matchesPattern(Pattern.compile("abc"), text));
-	}
-
-	@Test
-	public void matchesPattern_text_isValid() {
-		final String text = "abc";
-		Assert.assertSame(text, Check.matchesPattern(Pattern.compile("abc"), text));
-	}
-
-	@Test(expected = IllegalNullArgumentException.class)
-	public void matchesPattern_withArgName_chars_isNull() {
-		final String text = null;
-		Check.matchesPattern(Pattern.compile("abc"), text, "text");
-	}
-
-	@Test(expected = IllegalNullArgumentException.class)
-	public void matchesPattern_withArgName_pattern_isNull() {
-		final String text = "abc";
-		Check.matchesPattern(null, text, "text");
-	}
-
-	@Test(expected = IllegalPatternArgumentException.class)
-	public void matchesPattern_withArgName_stringbuilder_isInvalid() {
-		final StringBuilder builder = new StringBuilder();
-		builder.append("xyz");
-		Check.matchesPattern(Pattern.compile("abc"), builder, "builder");
-	}
-
-	@Test
-	public void matchesPattern_withArgName_stringbuilder_isValid() {
-		final StringBuilder builder = new StringBuilder();
-		builder.append("abc");
-		Check.matchesPattern(Pattern.compile("abc"), builder, "builder");
-	}
-
-	@Test
-	public void matchesPattern_withArgName_text_isValid() {
-		final String text = "abc";
-		Assert.assertSame(text, Check.matchesPattern(Pattern.compile("abc"), text, "text"));
 	}
 
 	@Test
