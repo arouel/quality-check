18a3b72740de81601c092c23c2c2377f4df645ad
André Rouél <andre.rouel@googlemail.com>
2013-02-10 21:54:50 +0100

    Removed unnecessary code since new rendering engine is available

--- modules/quality-immutable-object/src/main/java/net/sf/qualitycheck/immutableobject/generator/ImmutableObjectGenerator.java
+++ modules/quality-immutable-object/src/main/java/net/sf/qualitycheck/immutableobject/generator/ImmutableObjectGenerator.java
@@ -7,7 +7,6 @@ import japa.parser.ast.body.TypeDeclaration;
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
@@ -15,7 +14,6 @@ import javax.annotation.concurrent.Immutable;
 import net.sf.qualitycheck.Check;
 import net.sf.qualitycheck.immutableobject.domain.Abstract;
 import net.sf.qualitycheck.immutableobject.domain.Annotation;
-import net.sf.qualitycheck.immutableobject.domain.Attribute;
 import net.sf.qualitycheck.immutableobject.domain.Clazz;
 import net.sf.qualitycheck.immutableobject.domain.Constructor;
 import net.sf.qualitycheck.immutableobject.domain.Field;
@@ -25,11 +23,9 @@ import net.sf.qualitycheck.immutableobject.domain.Imports;
 import net.sf.qualitycheck.immutableobject.domain.Interface;
 import net.sf.qualitycheck.immutableobject.domain.Method;
 import net.sf.qualitycheck.immutableobject.domain.Package;
-import net.sf.qualitycheck.immutableobject.domain.ReturnType;
 import net.sf.qualitycheck.immutableobject.domain.Static;
 import net.sf.qualitycheck.immutableobject.domain.Type;
 import net.sf.qualitycheck.immutableobject.domain.Visibility;
-import net.sf.qualitycheck.immutableobject.parser.InterfaceParser;
 import net.sf.qualitycheck.immutableobject.util.FieldUtil;
 import net.sf.qualitycheck.immutableobject.util.SourceCodeFormatter;
 
@@ -42,18 +38,6 @@ public final class ImmutableObjectGenerator {
 
 	public static final String CLAZZ_PREFIX = "Immutable";
 
-	private static final List<Attribute> WITHOUT_ATTRIBUTES = ImmutableList.of();
-
-	@Nonnull
-	private static List<Annotation> findAnnotations(@Nonnull final java.lang.annotation.Annotation[] annotations) {
-		Check.notNull(annotations, "annotations");
-		final ImmutableList.Builder<Annotation> builder = new ImmutableList.Builder<Annotation>();
-		for (final java.lang.annotation.Annotation annotation : annotations) {
-			builder.add(Annotation.of(annotation));
-		}
-		return builder.build();
-	}
-
 	@Nonnull
 	private static List<Field> findFields(@Nonnull final List<Method> methods) {
 		Check.notNull(methods, "methods");
@@ -64,20 +48,6 @@ public final class ImmutableObjectGenerator {
 					Final.FINAL, Static.UNDEFINED, annotations, Field.WITHOUT_VALUE, FieldUtil.determineAccessorPrefix(method.getName())));
 		}
 		return fields;
-	}
-
-	@Nonnull
-	private static List<Method> findMethods(@Nonnull final Class<?> type) {
-		final Set<java.lang.reflect.Method> methods = InterfaceParser.findReadableMethods(type);
-		final List<Method> members = Lists.newArrayList();
-		for (final java.lang.reflect.Method method : methods) {
-			final List<Annotation> annotations = findAnnotations(method.getAnnotations());
-			final ReturnType returnType = ReturnType.of(method.getReturnType());
-			final Method m = new Method(method.getName(), returnType, Method.NOT_IMPLEMENTED, WITHOUT_ATTRIBUTES, Visibility.PUBLIC,
-					Final.UNDEFINED, Static.UNDEFINED, annotations);
-			members.add(m);
-		}
-		return members;
 	}
 
 	@Nonnull
@@ -102,8 +72,7 @@ public final class ImmutableObjectGenerator {
 			final CompilationUnit unit = JavaParser.parse(new ByteArrayInputStream(code.getBytes(Charsets.UTF_8.displayName())));
 			final List<TypeDeclaration> types = unit.getTypes();
 			final TypeDeclaration type = types.get(0);
-			Imports imports = SourceCodeReader.findImports(unit.getImports()).copyAndAdd(EqualsMethodGenerator.DEPENDS_ON)
-					.copyAndAdd(HashCodeMethodGenerator.DEPENDS_ON);
+			final Imports imports = SourceCodeReader.findImports(unit.getImports());
 			final String name = CLAZZ_PREFIX + type.getName();
 			final Package pkg = new Package(unit.getPackage().getName().toString());
 			final List<Annotation> annotations = SourceCodeReader.findAnnotations(type.getAnnotations(), imports);
@@ -113,13 +82,7 @@ public final class ImmutableObjectGenerator {
 			fields.add(SerialVersionGenerator.generate());
 			fields.addAll(findFields(methods));
 
-			methods.add(EqualsMethodGenerator.generate(name, fields));
-			methods.add(HashCodeMethodGenerator.generate(fields));
-
-			final ConstructorGenerator constructorGenerator = new ConstructorGenerator(name, fields);
-			final Constructor constructor = constructorGenerator.createConstructor();
-			final List<Constructor> constructors = ImmutableList.of(constructor);
-			imports = imports.copyAndAdd(constructorGenerator.dependsOn());
+			final List<Constructor> constructors = ImmutableList.of();
 			final List<Interface> interfaces = ImmutableList.of(new Interface(new Type(pkg, type.getName(), GenericDeclaration.UNDEFINED)));
 			clazz = new Clazz(name, pkg, fields, constructors, methods, Visibility.PUBLIC, Final.FINAL, Abstract.UNDEFINED, interfaces,
 					imports.asList(), annotations);
@@ -128,56 +91,6 @@ public final class ImmutableObjectGenerator {
 		}
 
 		return SourceCodeFormatter.format(ImmutableObjectRenderer.toString(clazz));
-	}
-
-	/**
-	 * The specified interface must not contain methods, that changes the state of this object itself.
-	 * 
-	 * @param type
-	 *            an interface which describes how to generate the <i>immutable</i>
-	 * @return generated source code as string
-	 */
-	public static String generateByInterface(@Nonnull final Class<?> type) {
-		Check.notNull(type, "type");
-		Check.stateIsTrue(type.isInterface(), "requires an interface");
-		Check.stateIsTrue(!hasPossibleMutatingMethods(type), "The passed interface '%s' seems to have mutating methods", type);
-
-		final String name = CLAZZ_PREFIX + type.getSimpleName();
-		final Package pkg = new Package(type.getPackage().getName());
-
-		final List<Method> methods = generateAccessorMethods(findMethods(type));
-		final List<Field> fields = new ArrayList<Field>();
-		fields.add(SerialVersionGenerator.generate());
-		fields.addAll(findFields(methods));
-		final ConstructorGenerator constructorGenerator = new ConstructorGenerator(name, fields);
-		final Constructor constructor = constructorGenerator.createConstructor();
-		final List<Constructor> constructors = ImmutableList.of(constructor);
-
-		methods.add(EqualsMethodGenerator.generate(name, fields));
-		methods.add(HashCodeMethodGenerator.generate(fields));
-
-		final Final finalModifier = Final.FINAL;
-		final Abstract abstractModifier = Abstract.UNDEFINED;
-		final Visibility visibility = Visibility.PUBLIC;
-		final List<Interface> interfaces = ImmutableList.of(Interface.of(type));
-		final Imports imports = EqualsMethodGenerator.DEPENDS_ON.copyAndAdd(HashCodeMethodGenerator.DEPENDS_ON).copyAndAdd(
-				constructorGenerator.dependsOn());
-		final List<Annotation> annotations = ImmutableList.of(Annotation.IMMUTABLE);
-		final Clazz clazz = new Clazz(name, pkg, fields, constructors, methods, visibility, finalModifier, abstractModifier, interfaces,
-				imports.asList(), annotations);
-
-		return SourceCodeFormatter.format(ImmutableObjectRenderer.toString(clazz));
-	}
-
-	/**
-	 * Checks if the given type has possible mutating methods (e.g. setter methods or methods with parameters).
-	 * 
-	 * @param type
-	 *            interface
-	 * @return {@code true} if possible mutating methods were found otherwise {@code false}
-	 */
-	private static boolean hasPossibleMutatingMethods(@Nonnull final Class<?> type) {
-		return !InterfaceParser.findPossibleMutatingMethods(type).isEmpty();
 	}
 
 }
--- modules/quality-immutable-object/src/test/java/net/sf/qualitycheck/immutableobject/ImmutableObjectGeneratorTest.java
+++ modules/quality-immutable-object/src/test/java/net/sf/qualitycheck/immutableobject/ImmutableObjectGeneratorTest.java
@@ -8,37 +8,29 @@ import java.io.InputStreamReader;
 
 import net.sf.qualitycheck.immutableobject.generator.ImmutableObjectGenerator;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.io.CharStreams;
 
 public class ImmutableObjectGeneratorTest {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ImmutableObjectGeneratorTest.class);
-
-	@Test
-	@Ignore("This is only a convenience test to get a quick overview what differ.")
-	public void compareReadingSourceCodeVersusRuntime() throws IOException {
-		final InputStream stream = this.getClass().getClassLoader().getResourceAsStream("Car.java");
-		final String code = CharStreams.toString(new InputStreamReader(stream));
-		final Class<?> type = Car.class;
-		assertEquals(ImmutableObjectGenerator.generateByInterface(type), ImmutableObjectGenerator.generateByCode(code));
-	}
-
 	@Test
 	public void createByReadingSourceCode() throws IOException {
-		final InputStream stream = this.getClass().getClassLoader().getResourceAsStream("Car.java");
-		final String code = CharStreams.toString(new InputStreamReader(stream));
-		LOG.info(ImmutableObjectGenerator.generateByCode(code));
+		readInterfaceAndCompareToReference("Car.java");
 	}
 
-	@Test
-	public void createDuringRuntime() {
-		final Class<?> type = Car.class;
-		LOG.info(ImmutableObjectGenerator.generateByInterface(type));
+	private String readInterface(final String name) throws IOException {
+		final InputStream stream = getClass().getClassLoader().getResourceAsStream(name);
+		return CharStreams.toString(new InputStreamReader(stream));
+	}
+
+	private void readInterfaceAndCompareToReference(final String name) throws IOException {
+		assertEquals(readReferenceImmutable(name), ImmutableObjectGenerator.generateByCode(readInterface(name)));
+	}
+
+	private String readReferenceImmutable(final String name) throws IOException {
+		final InputStream stream = getClass().getClassLoader().getResourceAsStream("Immutable" + name);
+		return CharStreams.toString(new InputStreamReader(stream));
 	}
 
 }
