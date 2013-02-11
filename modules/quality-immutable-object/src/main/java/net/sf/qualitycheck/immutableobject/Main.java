2d270fd4f37e8fcca414571c061aff4d22e90cf8
André Rouél <andre.rouel@googlemail.com>
2013-02-11 23:03:59 +0100

    Added default settings to be able to render an immutable object class with builder

--- modules/quality-immutable-object/src/main/java/net/sf/qualitycheck/immutableobject/Main.java
+++ modules/quality-immutable-object/src/main/java/net/sf/qualitycheck/immutableobject/Main.java
@@ -3,6 +3,7 @@ package net.sf.qualitycheck.immutableobject;
 import java.io.File;
 
 import net.sf.qualitycheck.Check;
+import net.sf.qualitycheck.immutableobject.domain.ImmutableSettings;
 import net.sf.qualitycheck.immutableobject.generator.ImmutableObjectGenerator;
 
 import org.slf4j.Logger;
@@ -14,6 +15,27 @@ import com.google.common.io.Files;
 public class Main {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	static ImmutableSettings defaultSettings() {
+		final ImmutableSettings.Builder settings = new ImmutableSettings.Builder();
+
+		// global settings
+		settings.jsr305Annotations(true);
+		settings.guava(false);
+		settings.qualityCheck(true);
+
+		// immutable settings
+		settings.serializable(false);
+
+		// builder settings
+		settings.builderCopyConstructor(true);
+		settings.builderFlatMutators(false);
+		settings.builderFluentMutators(true);
+		settings.builderName("Builder");
+		settings.builderImplementsInterface(false);
+
+		return settings.build();
+	}
 
 	public static void main(final String[] args) throws Exception {
 		Check.notNull(args, "args");
@@ -27,11 +49,12 @@ public class Main {
 		for (final String path : args) {
 			final File file = new File(path);
 			final String code = Files.toString(file, Charsets.UTF_8);
-			final String generated = ImmutableObjectGenerator.generate(code, null);
+			final String generated = ImmutableObjectGenerator.generate(code, defaultSettings());
 			LOG.info("\n" + generated);
 		}
 		LOG.info("--");
 		LOG.info("We hope that you saved some time.");
 		LOG.info("Your participation in this project is much appreciated!");
 	}
+
 }
