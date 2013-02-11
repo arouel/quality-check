97c59a6a8368518ec78d943eac12bf25530a3710
André Rouél <andre.rouel@googlemail.com>
2013-02-11 18:44:26 +0100

    Removed unused arguments when rendering the immutable object compilation unit

--- modules/quality-immutable-object/src/main/java/net/sf/qualitycheck/immutableobject/generator/ImmutableObjectRenderer.java
+++ modules/quality-immutable-object/src/main/java/net/sf/qualitycheck/immutableobject/generator/ImmutableObjectRenderer.java
@@ -1,35 +1,18 @@
 package net.sf.qualitycheck.immutableobject.generator;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 import net.sf.qualitycheck.Check;
-import net.sf.qualitycheck.immutableobject.domain.Annotation;
-import net.sf.qualitycheck.immutableobject.domain.Attribute;
 import net.sf.qualitycheck.immutableobject.domain.Clazz;
 import net.sf.qualitycheck.immutableobject.domain.Field;
-import net.sf.qualitycheck.immutableobject.domain.Final;
 import net.sf.qualitycheck.immutableobject.domain.ImmutableSettings;
-import net.sf.qualitycheck.immutableobject.domain.Interface;
 import net.sf.qualitycheck.immutableobject.util.SourceCodeFormatter;
 
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 import org.stringtemplate.v4.STGroupFile;
 
-import com.google.common.collect.Lists;
-
 final class ImmutableObjectRenderer {
-
-	@Nullable
-	private static Attribute determineCopyConstructorAttribute(@Nonnull final Clazz clazz) {
-		Attribute attr = null;
-		if (clazz.getInterfaces().size() == 1) {
-			final Interface i = clazz.getInterfaces().get(0);
-			attr = new Attribute(i.getType().getName().toLowerCase(), i.getType(), Final.FINAL, Lists.newArrayList(Annotation.NONNULL));
-		}
-		return attr;
-	}
 
 	public static String toString(@Nonnull final Clazz clazz) {
 		Check.notNull(clazz, "clazz");
@@ -52,18 +35,13 @@ final class ImmutableObjectRenderer {
 
 		// builder settings
 		settings.builderCopyConstructor(true);
-		settings.builderFlatMutators(true);
+		settings.builderFlatMutators(false);
 		settings.builderFluentMutators(true);
 		settings.builderName("Builder");
 		settings.builderSameInterface(false);
 
-		template.add("builderName", "Builder");
-		template.add("builderNotThreadSafeAnnotation", Annotation.NOT_THREAD_SAFE);
-		template.add("clazz", clazz);
-		template.add("copyConstructorAttribute", determineCopyConstructorAttribute(clazz));
-		template.add("immutableAnnotation", Annotation.IMMUTABLE);
-		template.add("immutableName", clazz.getName());
 		template.add("settings", settings.build());
+
 		return SourceCodeFormatter.format(template.render());
 	}
 
