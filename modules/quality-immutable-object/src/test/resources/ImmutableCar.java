b123235294087bdeed4c185db587fe64fcd6dd08
André Rouél <arouel@datameer.com>
2013-02-12 19:56:16 +0100

    Updated test expectation

--- modules/quality-immutable-object/src/test/resources/ImmutableCar.java
+++ modules/quality-immutable-object/src/test/resources/ImmutableCar.java
@@ -37,9 +37,9 @@ public final class ImmutableCar implements Car {
 		}
 
 		public Builder(@Nonnull final Car car) {
-			this.codes = new ArrayList<String>(Check.notNull(car.getCodes, "car.getCodes"));
-			this.wheels = Check.notNegative(car.getWheels, "car.getWheels");
-			this.name = Check.notNull(car.getName, "car.getName");
+			this.codes = new ArrayList<String>(Check.notNull(car.getCodes(), "car.getCodes()"));
+			this.wheels = Check.notNegative(car.getWheels(), "car.getWheels()");
+			this.name = Check.notNull(car.getName(), "car.getName()");
 		}
 
 		@Nonnull
