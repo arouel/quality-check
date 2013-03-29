b4c0fe8d03c4ea6c9459df65102709d16daf8b2d
André Rouél <andre.rouel@googlemail.com>
2013-03-29 15:09:44 +0100

    Changes to avoid conflicts when measuring code coverage with JaCoCo
    JaCoCo adds a static private method $jacocoinit() which is added to
    every non-interface class. This breaks the expected test result and
    therefore we should analyze classes of another package or better of
    another library.

--- modules/quality-test/src/test/java/net/sf/qualitytest/StaticCheckTest_publicMethodsAnnotated.java
+++ modules/quality-test/src/test/java/net/sf/qualitytest/StaticCheckTest_publicMethodsAnnotated.java
@@ -1,5 +1,9 @@
 package net.sf.qualitytest;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 import net.sf.qualitytest.exception.IllegalMissingAnnotationOnMethodException;
 
 import org.junit.Test;
@@ -8,11 +12,15 @@ public class StaticCheckTest_publicMethodsAnnotated {
 
 	@Test
 	public void testAllMethodsAreAnnotated() {
-		StaticCheck.publicMethodsAnnotated(StaticCheckTest_publicMethodsAnnotated.class, Test.class);
+		StaticCheck.publicMethodsAnnotated(Documented.class, Retention.class);
+		StaticCheck.publicMethodsAnnotated(Documented.class, Target.class);
+		StaticCheck.publicMethodsAnnotated(Override.class, Retention.class);
+		StaticCheck.publicMethodsAnnotated(Override.class, Target.class);
 	}
-	
-	@Test(expected=IllegalMissingAnnotationOnMethodException.class)
+
+	@Test(expected = IllegalMissingAnnotationOnMethodException.class)
 	public void testAllMethodsAreAnnotated_error() {
-		StaticCheck.publicMethodsAnnotated(StaticCheck.class, Test.class);
+		StaticCheck.publicMethodsAnnotated(Retention.class, Retention.class);
 	}
+
 }
