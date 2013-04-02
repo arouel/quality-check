f4c8a78fd1866238630ee924d139ed44927881a3
Dominik Seichter <domseichter@web.de>
2013-04-02 18:23:33 +0200

    Added Unit-Tests for creation-strategies

---
+++ modules/quality-test/src/test/java/net/sf/qualitytest/blueprint/strategy/creation/RandomCharValueCreationStrategyTest.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright 2013 Dominik Seichter
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package net.sf.qualitytest.blueprint.strategy.creation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RandomCharValueCreationStrategyTest {
+
+	@Test
+	public void testCreationStrategy() {
+		Assert.assertNotNull(new RandomCharValueCreationStrategy().createValue());
+	}
+}
