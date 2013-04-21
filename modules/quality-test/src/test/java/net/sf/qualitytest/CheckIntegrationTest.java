013f66cfc61aca70468c4dac3d4628ab14fbf12f
Dominik Seichter <domseichter@web.de>
2013-04-21 14:54:48 +0200

    Added a list of ignored methods to CheckIntegrationTest so that we can skip the method "nothing" on purpose

--- modules/quality-test/src/test/java/net/sf/qualitytest/CheckIntegrationTest.java
+++ modules/quality-test/src/test/java/net/sf/qualitytest/CheckIntegrationTest.java
@@ -1,15 +1,36 @@
 package net.sf.qualitytest;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
 import net.sf.qualitycheck.Check;
 import net.sf.qualitycheck.Throws;
+import net.sf.qualitytest.exception.IllegalMissingAnnotationOnMethodException;
 
 import org.junit.Test;
 
 public class CheckIntegrationTest {
 
-	@Test
-	public void publicMethodsMustBeAnnotatedWithThrows() {
-		StaticCheck.publicMethodsAnnotated(Check.class, Throws.class);
+	private static void publicMethodsAnnotated(@Nonnull final Class<?> clazz, @Nonnull final Class<? extends Annotation> annotation,
+			final List<String> ignoreList) {
+		final Method[] methods = clazz.getDeclaredMethods();
+		for (final Method m : methods) {
+			if (!ignoreList.contains(m.getName()) && Modifier.isPublic(m.getModifiers()) && !m.isAnnotationPresent(annotation)) {
+				throw new IllegalMissingAnnotationOnMethodException(clazz, annotation, m);
+			}
+		}
 	}
 
+	@Test
+	public void publicMethodsMustBeAnnotatedWithThrows() {
+		final List<String> ignoreList = new ArrayList<String>();
+		ignoreList.add("nothing");
+
+		publicMethodsAnnotated(Check.class, Throws.class, ignoreList);
+	}
 }
