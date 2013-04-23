83dd71416ff0c3b14fa9f667ffd8b248a3a8fccc
André Rouél <andre.rouel@googlemail.com>
2013-04-23 22:58:42 +0200

    Added test cases for method 'Check.notNegative' for values of type long (belongs to issue #1)

--- modules/quality-check/src/test/java/net/sf/qualitycheck/CheckTest_notNegative.java
+++ modules/quality-check/src/test/java/net/sf/qualitycheck/CheckTest_notNegative.java
@@ -8,31 +8,59 @@ import org.junit.Test;
 public class CheckTest_notNegative {
 
 	@Test
-	public void notNegative_checkReferenceIsSame() {
+	public void notNegative_int_checkReferenceIsSame() {
 		final int prime = 7;
 		Assert.assertSame(prime, Check.notNegative(7));
 		Assert.assertSame(prime, Check.notNegative(7, "prime"));
 	}
 
 	@Test(expected = IllegalNegativeArgumentException.class)
-	public void notNegative_withReference_isInvalid() {
+	public void notNegative_int_withReference_isInvalid() {
 		Check.notNegative(-1);
 	}
 
 	@Test
-	public void notNegative_withReference_isValid() {
+	public void notNegative_int_withReference_isValid() {
 		Check.notNegative(0);
 		Check.notNegative(Integer.MAX_VALUE);
 	}
 
 	@Test(expected = IllegalNegativeArgumentException.class)
-	public void notNegative_withReference_withName_isInvalid() {
+	public void notNegative_int_withReference_withName_isInvalid() {
 		Check.notNegative(Integer.MIN_VALUE, "min");
 	}
 
 	@Test
-	public void notNegative_withReference_withName_isValid() {
+	public void notNegative_int_withReference_withName_isValid() {
 		Check.notNegative(1, "one");
+	}
+
+	@Test
+	public void notNegative_long_checkReferenceIsSame() {
+		final long prime = 7L;
+		Assert.assertSame(prime, Check.notNegative(7L));
+		Assert.assertSame(prime, Check.notNegative(7L, "prime"));
+	}
+
+	@Test(expected = IllegalNegativeArgumentException.class)
+	public void notNegative_long_withReference_isInvalid() {
+		Check.notNegative(-1L);
+	}
+
+	@Test
+	public void notNegative_long_withReference_isValid() {
+		Check.notNegative(0L);
+		Check.notNegative(Long.MAX_VALUE);
+	}
+
+	@Test(expected = IllegalNegativeArgumentException.class)
+	public void notNegative_long_withReference_withName_isInvalid() {
+		Check.notNegative(Long.MIN_VALUE, "min");
+	}
+
+	@Test
+	public void notNegative_long_withReference_withName_isValid() {
+		Check.notNegative(1L, "one");
 	}
 
 }
